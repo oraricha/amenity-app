528f185d1e183b31437301186ae3f52e5f0245e7
oraricha <orarich@gmail.com>
2017-08-31 00:47:29 +0300

    added stock generic request

--- src/containers/stock.js
+++ src/containers/stock.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react';
+// import fetch from 'whatwg-fetch'
 
-export default () => (
-    <div>
-        <h1>About Us stock</h1>
-        <p>Hello Medium!</p>
-    </div>
-)
+const getData = () => {
+    fetch('https://www.quandl.com/api/v3/datasets/WIKI/goog/data.json')
+        .then((response) => {
+            console.log(response);
+            return response;
+        });
+};
+
+// export default () => (
+//     <div>
+//         <h1>About Us stock</h1>
+//         <p>Hello Medium!</p>
+//     </div>
+// )
+
+class Stock extends Component {
+    getData() {
+        debugger;
+        console.log(fetch);
+        return fetch('https://www.quandl.com/api/v3/datasets/WIKI/goog/data.json')
+            .then((response) => {
+                return response.json().then(function(data) {
+                    // do something with your data
+                    // console.log(data);
+                    return data;
+                });
+                // console.log(response.json());
+                // return response;
+            });
+    }
+
+    componentDidMount() {
+        this.getData().then((data) => {
+            console.log(data);
+        });
+    }
+
+    render() {
+        return (
+            <div className="stock-screen">
+                <div className="App-header">
+                    {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Stock;
